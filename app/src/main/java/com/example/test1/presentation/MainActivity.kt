df28a1e73ed94468dfb7d807627ef0f38c5ba582
Антон Януков <goliaf551@mail.ru>
2022-04-29 06:58:02 +0600

    Koin

--- app/src/main/java/com/example/test1/presentation/MainActivity.kt
+++ app/src/main/java/com/example/test1/presentation/MainActivity.kt
@@ -5,35 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.test1.R
-import com.example.test1.data.repository.UserRepositoryImpl
-import com.example.test1.data.storage.sharedprefs.SharedPrefUserStorage
-import com.example.test1.domain.models.SaveUserNameParam
-import com.example.test1.domain.models.UserName
-import com.example.test1.domain.usecase.GetUserNameUseCase
-import com.example.test1.domain.usecase.SaveUserNameUseCase
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext))
-    }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        GetUserNameUseCase(userRepository = userRepository)
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        SaveUserNameUseCase(userRepository = userRepository)
-    }
 
-    private lateinit var vm: MainViewModel
+    private val vm by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        vm = ViewModelProvider(this, MainViewModelFactory(this))
-            .get(MainViewModel::class.java)
 
         val dataTextView = findViewById<TextView>(R.id.tv_data)
         val dataEditView = findViewById<EditText>(R.id.et_data)
